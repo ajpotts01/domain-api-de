2eefae55f6d101d2a555659e68c43b4d8dd2491c
Nicholas Tantra <nick@rinlabs.com>
2022-10-06 09:43:55 +0700

    Converted ETL folders to PY files

--- src/domain/secrets_config.template.py
+++ src/domain/secrets_config.template.py
@@ -1,5 +1,15 @@
 api_key="key_zzz"
 
+source_db_user="zzz"
+source_db_password="zzz"
+source_db_server_name="zzz"
+source_db_database_name="zzz"
+
+target_db_user="zzz"
+target_db_password="zzz"
+target_db_server_name="zzz"
+target_db_database_name="zzz"
+
 db_user="zzz"
 db_password="zzz"
 db_server_name="zzz"
