0651a3021ddea810d6bd5cf0f4d697ffb0f55f3e
ajpotts03 <alex.potts@data-driven.com>
2022-10-07 21:19:25 +1100

    Storing current progress in case of a botched merge

--- src/domain/etl/transform.py
+++ src/domain/etl/transform.py
@@ -21,5 +21,26 @@ class Transform():
         self.db_engine = db_engine
         self.model_path = model_path
 
-    def run(self):
-        pass
+    def run(self) -> bool:
+        """
+        Builds models with a matching file name in the models_path folder. 
+        - `model`: the name of the model (without .sql)
+        - `models_path`: the path to the models directory containing the sql files. defaults to `models/`
+        """
+    
+        if f"{self.model}.sql" in os.listdir(self.model_path):
+            logging.info(f"Building model: {self.model}")
+        
+            # read sql contents into a variable 
+            with open(f"models/{self.model}.sql") as f: 
+                raw_sql = f.read()
+
+            # parse sql using jinja 
+            parsed_sql = j2.Template(raw_sql).render(target_table = self.model, engine=self.db_engine)
+
+            # execute parsed sql 
+            self.db_engine.execute(parsed_sql)
+            logging.info(f"Successfully built model: {self.model}")
+            return True 
+        else: 
+            logging.error(f"Could not find model: {self.model}")        
